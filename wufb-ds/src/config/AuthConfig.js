63db8c91144cb48682cc55c303e3e5f8978e8158
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2023-04-08 04:04:42 +0000

    fix: wufb-ds redirect URI

--- wufb-ds/src/config/AuthConfig.js
+++ wufb-ds/src/config/AuthConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: "0fa38708-b7d4-4600-bc07-2b1b6a329e4e", // e.g. "70258689-8a4e-410f-a300-cb2011f23cf3"
     authority: "https://login.windows.net/9bccba71-9d01-4474-bae1-d55da0d9b8f6", //e.g. "https://login.windows.net/4d67e6d4-78f7-438b-8e40-7e408dcfa0ca"
-    redirectUri: "http://localhost:3000",
+    redirectUri: "https://wufb-ds.tplant.com.au",
   },
   cache: {
     cacheLocation: "sessionStorage",
